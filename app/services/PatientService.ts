e1f76c3fdf69a981048844b605546d69119aacb2
Tharit Wichiankoo <tharit@bonmek.com>
2020-01-15 10:22:32 +0700

    [Update] PatientService

--- app/services/PatientService.ts
+++ app/services/PatientService.ts
@@ -2,6 +2,7 @@ import IAdapter from '@adapters/IAdapter'
 import DataManager from '@data-managers/DataManager'
 import PatientDataManager from '@data-managers/PatientDataManager'
 import ValidatorManager from '@validators/ValidatorManager'
+import * as _ from 'lodash'
 import AbstractService from './AbstractService'
 
 export default class PatientService extends AbstractService {
@@ -12,15 +13,18 @@ export default class PatientService extends AbstractService {
   async resourceList(params?: any): Promise<any> {
     console.info(`[service] loading resource resourceList`, params)
     const dataManager = this.dataManager as PatientDataManager
-    const result = await dataManager.resourceList(params || {})
+    const result = await dataManager.resourceList(params.id, params || {})
     return {
       ...result,
-      data: result.data.map((result: any) => {
+      data: _.map(result.data, (result: any) => {
         const validator = ValidatorManager.compile(result.schema)
         if (validator) {
+          const test = _.map(result.data, (entry: any) => {
+            return validator.parse(entry)
+          })
           return {
             ...result,
-            data: result.data.map((entry: any) => {
+            data: _.map(result.data, (entry: any) => {
               return validator.parse(entry)
             }),
           }
