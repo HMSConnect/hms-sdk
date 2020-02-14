d9505a4f9261685340c3aa1389eca8e2c4f1e3ab
poon-bmk <tharit@bonmek.com>
2020-02-14 11:37:29 +0700

    [Update] history graph ui

--- app/components/base/GraphBase.tsx
+++ app/components/base/GraphBase.tsx
@@ -19,6 +19,9 @@ import { area, curveCatmullRom, symbol, symbolCircle } from 'd3-shape'
 import * as _ from 'lodash'
 import * as moment from 'moment'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import { Tooltip as TooltipMatterial } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles'
+
 interface IGraphLineOption {
   includeLegend?: boolean
   customPrepareGraphData?: (data: any) => any
@@ -97,37 +100,57 @@ const LegendRoot: React.FunctionComponent<Legend.RootProps> = (
         flexFlow: 'row wrap',
         margin: 'auto',
         width: '100%',
-        // WebkitDisplay: '-webkit-box -webkit-flex'
+        maxHeight: '15em',
+        overflow: 'auto'
       }}
     />
   )
 }
 
+const LightTooltip = withStyles(theme => ({
+  tooltip: {
+    backgroundColor: '#555',
+    color: '#fff',
+    boxShadow: theme.shadows[1],
+    fontSize: '0.8rem',
+  },
+}))(TooltipMatterial);
+
 const LegendItem: React.FunctionComponent<Legend.ItemProps> = (
   props: Legend.ItemProps,
 ) => {
   return (
-    <Legend.Item
-      {...props}
-      style={{
-        width: '100px',
-        flexDirection: 'column',
-        marginLeft: '-2px',
-        marginRight: '-2px',
-      }}
-    />
+    <>
+      <LightTooltip title={_.get(props, 'children[0].props.name')} style={{ fontSize: '0.8rem' }}>
+        <Legend.Item
+          {...props}
+          style={{
+            width: '100px',
+            flexDirection: 'column',
+            marginLeft: '-2px',
+            marginRight: '-2px',
+          }}
+        />
+      </LightTooltip>
+
+    </>
   )
+}
+
+const LegendLabel: React.FunctionComponent<Legend.LabelProps> = (props) => {
+  return <Legend.Label {...props} style={{
+    WebkitBoxOrient: 'vertical',
+    WebkitLineClamp: 2,
+    display: '-webkit-box',
+    overflow: 'hidden',
+    fontSize: '0.8rem'
+  }} />
 }
 
 const ValueLabel = (props: any) => {
   const { text, unit } = props
   return <ValueAxis.Label {...props} text={`${text} ${unit || ''}`} />
 }
-
-// const ValueLabel = (props: any, unit: any) => (props: any) => {
-//   const { text, unit } = props
-//   return <ValueAxis.Label {...props} text={`${text} ${unit || ''}`} />
-// }
 
 const Point = (type: any, styles: any) => (props: any) => {
   const { arg, val, color } = props
@@ -178,6 +201,36 @@ const DashLineWithCirclePoint: React.FunctionComponent<any> = (props: any) => {
     </>
   )
 }
+const TooltipArrow: React.FunctionComponent<Tooltip.ArrowProps> = (props) => {
+  return <Tooltip.Arrow {...props} style={{
+    color: '#555',
+  }} />
+}
+
+const TooltipOverley: React.FunctionComponent<Tooltip.OverlayProps> = (props) => {
+  return <Tooltip.Overlay {...props} style={{
+    backgroundColor: '#555',
+    position: 'absolute',
+    borderRadius: '6px',
+    marginLeft: '-60px',
+    bottom: '125%',
+    left: '50%',
+    padding: '0.5em'
+  }} />
+}
+
+const ToolTipSheet: React.FunctionComponent<Tooltip.SheetProps> = (props) => {
+  return <Tooltip.Sheet {...props} style={{
+    backgroundColor: '#555',
+    color: '#fff',
+    opacity: 1,
+    padding: '5px 0',
+    textAlign: 'center',
+    transition: 'opacity 0.3s',
+    width: '100%',
+    zIndex: 1,
+  }} />
+}
 
 const TooltipContent: React.FunctionComponent<{
   graphData: any
@@ -186,25 +239,29 @@ const TooltipContent: React.FunctionComponent<{
   const TooltipContentComponent: React.FunctionComponent<any> = props => {
     const targetElement = props.targetItem
     return (
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <div className={classes.tooltiptext}>
-          {_.map(graphData[targetElement.point], (value, key) => (
-            <Typography variant='body2' key={`tooltipText${key}`}>
-              <>
-                {_.truncate(_.startCase(key), { length: 15 })} :
+      // <div style={{ display: 'flex', flexDirection: 'column' }}>
+      //   <div className={classes.tooltiptext}>
+      //     {
+      <div >
+        {_.map(graphData[targetElement.point], (value, key) => (
+          <Typography variant='body2' key={`tooltipText${key}`}>
+            <>
+              {_.truncate(_.startCase(key), { length: 40 })} :
                 {_.isDate(value)
-                  ? moment
-                    .default(value)
-                    .format(environment.localFormat.dateTime)
-                  : Number(value).toFixed(2)}
-              </>
-            </Typography>
-          ))}
-        </div>
+                ? moment
+                  .default(value)
+                  .format(environment.localFormat.dateTime)
+                : Number(value).toFixed(2)}
+            </>
+          </Typography>
+        ))}
       </div>
+      //     }
+      //    </div>
+      // </div> 
     )
   }
-  return <Tooltip contentComponent={TooltipContentComponent} />
+  return <Tooltip overlayComponent={TooltipOverley} sheetComponent={ToolTipSheet} contentComponent={TooltipContentComponent} />
 }
 
 const GraphBase: React.FunctionComponent<{
@@ -394,6 +451,7 @@ export const GraphLine: React.FunctionComponent<{
               markerComponent={(props: any) => <Marker {...props} />}
               rootComponent={LegendRoot}
               itemComponent={LegendItem}
+              labelComponent={LegendLabel}
             />
           ) : (
             <Legend
