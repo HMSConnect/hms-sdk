7d3d23019582801e062cba520bb074b05270982b
Tharit Wichiankoo <tharit@bonmek.com>
2020-04-30 10:40:07 +0700

    [Update] gallery image & usage image

--- app/components/templates/landing/views/Usage.tsx
+++ app/components/templates/landing/views/Usage.tsx
@@ -63,7 +63,6 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     image: {
       height: 300,
-      width: 300,
       marginTop: theme.spacing(4),
       marginBottom: theme.spacing(4),
     },
@@ -123,7 +122,7 @@ const Usage: React.FunctionComponent<any> = (props) => {
               className={clsx(classes.item, classes.paperContainer)}
             >
               <img
-                src='../../../../static/images/demographic.png'
+                src='../../../../static/images/demographicUsage.png'
                 alt='suitcase'
                 className={classes.image}
               />
