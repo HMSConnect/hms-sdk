00b82cee2be8e0e7cd37c9e189934bf113f4b08d
kopai-msi <kopai-sathin@hotmail.com>
2020-07-23 15:25:41 +0700

    [fixed] validator check resourceType

--- app/validators/standard/hms/2.4/HMSCarePlanV24XValidator.ts
+++ app/validators/standard/hms/2.4/HMSCarePlanV24XValidator.ts
@@ -1,11 +1,11 @@
 import IValidator from '@validators/IValidator'
-
+import { snakeCase } from 'lodash'
 class HMSCarePlanV24XValidator implements IValidator {
   isValid(schema: any): boolean {
     return (
       schema.standard === 'HMS' &&
       schema.version.startsWith('2.4') &&
-      schema.resourceType === null
+      snakeCase(schema.resourceType) === 'care_plan'
     )
   }
 
--- app/validators/standard/hms/2.4/HMSMedicationRequestV24xValidator.ts
+++ app/validators/standard/hms/2.4/HMSMedicationRequestV24xValidator.ts
@@ -5,17 +5,18 @@ class HMSMedicationRequestV24xValidator implements IValidator {
   isValid(schema: any): boolean {
     return (
       schema.standard === 'HMS' &&
-      schema.version.startsWith('2.4') 
-      // &&
-      // schema.resourceType === ''
+      schema.version.startsWith('2.4') &&
+      _.snakeCase(schema.resourceType) === 'medication_dispense'
     )
   }
 
   parse(data: any): any {
-    console.log(data);
     return {
       ...data,
-      medicationCodeableConcept: _.get(_.last(data.medication),'medicineGenericName')
+      medicationCodeableConcept: _.get(
+        _.last(data.medication),
+        'medicineGenericName',
+      ),
     }
   }
 }
